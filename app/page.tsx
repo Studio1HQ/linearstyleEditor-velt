e8927140f2cdd1a7aad2af76468e571e22259141
YashChopra2511 <chopray567@gmail.com>
2025-09-14 01:58:39 +0530

    changes for dark-mode theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { MainLayout } from "@/components/layout/MainLayout";
+import { ThemeProvider } from "@/hooks/useTheme";
 import { VeltComments, VeltProvider } from "@veltdev/react";
 
 export default function Home() {
   return (
     <VeltProvider apiKey={process.env.NEXT_PUBLIC_VELT_ID!}>
-      <MainLayout />
-      <VeltComments
-        textMode={false}
-        shadowDom={false}
-        textCommentToolShadowDom={false}
-      />
+      <ThemeProvider>
+        <MainLayout />
+        <VeltComments
+          textMode={false}
+          shadowDom={false}
+          textCommentToolShadowDom={false}
+        />
+      </ThemeProvider>
     </VeltProvider>
   );
 }
